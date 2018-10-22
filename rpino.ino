20a7c31d62dcff5fadea972a4cc2d3609354462c
Mauro Rappa <maurorappa@aol.co.uk>
2018-10-22 23:44:05 +0100

    added Arduino code

---
+++ rpino.ino
@@ -0,0 +1,75 @@
+
+#include <TroykaDHT.h>
+
+int DHTPin = 10;
+int LightPin = A0;
+int light = 0;
+int RainPin = A2;
+int rain = 0;
+DHT dht(DHTPin, DHT11);
+String incoming;
+String temp;
+String humidity;
+String dht_status;
+int ledPin = 13; 
+
+void setup() {
+  Serial.begin(9600);
+  incoming = "";
+  dht.begin();
+  Serial.println("RPino ready!");
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+}
+
+void loop() {
+  
+  if (Serial.available() > 0) {
+    incoming = Serial.readStringUntil("\n");
+    dht.read();
+    switch(dht.getState()) {
+      case DHT_OK:
+       temp = dht.getTemperatureC();
+       humidity = dht.getHumidity();
+       dht_status = "ok";
+       break;
+     case DHT_ERROR_CHECKSUM:
+       dht_status = "Checksum error";
+       break;
+     case DHT_ERROR_TIMEOUT:
+       dht_status = "Time out error";
+       break;
+     case DHT_ERROR_NO_REPLY:
+        dht_status = "Sensor not connected";
+       break;
+    }  
+    
+    if (incoming == "L?\n") {
+       light = analogRead(LightPin);
+       Serial.print("L: ");
+       Serial.println(light);
+    }
+    if (incoming == "R?\n") {
+       rain = analogRead(RainPin);
+       Serial.print("R: ");
+       Serial.println(rain);
+    }  
+    if (incoming == "T?\n") {
+       Serial.print("T: ");
+       Serial.println(temp);
+    }
+    if (incoming == "H?\n") {
+       Serial.print("H: ");
+       Serial.println(humidity);
+    }
+    if (incoming == "S?\n") {
+       Serial.print("S: ");
+       Serial.println(dht_status);
+       digitalWrite(ledPin, HIGH);
+      delay(1000);
+      digitalWrite(ledPin, LOW);
+    }
+  }  
+  //Serial.println();
+  delay(1000);
+}
